f8f008d618a6ce7ee24112a7558a3cad872894b4
Barry Evans <barry.p.evans@gmail.com>
2023-09-10 12:23:37 -0700

    lil bob

--- Assets/Scripts/Gameplay/HatPickup.cs
+++ Assets/Scripts/Gameplay/HatPickup.cs
@@ -7,6 +7,17 @@ namespace Gameplay
 
     public class HatPickup : MonoBehaviour
     {
+        private float _liveTime = 0f;
+        private float _bobRate = 1f;
+        private float _bobDist = 0.1f;
+        private Vector3 _initialPos;
+
+        private void Start()
+        {
+            _initialPos = transform.position;
+            _liveTime = Random.value;
+        }
+
         [SerializeField]
         HatType hatType;
         private void OnTriggerEnter(Collider other)
@@ -16,6 +27,12 @@ namespace Gameplay
                 other.GetComponent<HatMgr>().SetHat(hatType);
                 Destroy(gameObject);
             }
+        }
+
+        private void ClayUpdate()
+        {
+            _liveTime += Time.deltaTime * _bobRate;
+            transform.position = _initialPos + Vector3.up * Mathf.Sin(_liveTime) * _bobDist;
         }
     }
 }
